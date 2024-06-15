a47137ebf032f730afd94c5de38880b22d163c4a
junjunhee12 <44117412+junjunhee12@users.noreply.github.com>
2024-06-15 12:00:37 +0900

    (재시도 . 2)

---
+++ src/components/Education.js
@@ -0,0 +1,103 @@
+import { defer } from "react-router-dom";
+import { Aboutme, Aboutmecontent, Img, TitleBottom, TitleTob } from "./About";
+import { H3, H4, P } from "./Home";
+import styled from "styled-components";
+import { useState } from "react";
+import 표창장 from "../assets/학원3.jpg"
+import 수료증 from "../assets/학원.jpg" 
+import 이수증 from "../assets/학원2.jpg" 
+import 아카데미 from "../assets/아카데미.jpg" 
+const AcEducation = styled(Aboutme)`
+    /* background-color: dodgerblue; */
+`
+const EducationContent = styled(Aboutmecontent)`
+    /* background-color: yellow; */
+    justify-content: space-between;
+    align-items: center;
+`
+const EducationTitleTob = styled(TitleTob)`
+    justify-content: space-between;
+`
+const EducationTitleBottom = styled(TitleBottom)`
+
+`
+const EducationImg = styled.div`
+    width: 500px;
+    max-width: 1196px;
+    height: 650px;
+    /* background-color: yellowgreen; */
+    border-radius: 16px;
+    overflow: hidden;
+`
+
+const TitleTobcontent = styled.div`
+    height: 100%;
+    /* background-color: yellow; */
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    gap: 1rem;
+    padding-right: 12px;
+    cursor: pointer;
+`
+const ContentRight = styled.div`
+    width: 700px;
+    height: 650px;
+    /* background-color: yellow; */
+    border-radius: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+`
+
+const EdImg = styled.div`
+    width: 100%;
+    height: 600px;
+    background-color: yellowgreen;
+`
+
+const Edp = styled.p`
+    font-weight: 16px;
+`
+const img = [수료증, 이수증, 표창장 ]
+function Education(){
+
+    const [index, setIndex] = useState(0);
+
+    const handleIndexChange = (newIndex) => {
+        setIndex(newIndex);
+    };
+    return(
+        <>
+            <AcEducation>
+                <EducationTitleTob>
+                    <H3>Education &gt; </H3>
+                    <TitleTobcontent>
+                        <H4>&gt;</H4>
+                        <H4 onClick={() => handleIndexChange(0)}>수료증</H4>
+                        <H4 onClick={() => handleIndexChange(1)} >이수증</H4>
+                        <H4 onClick={() => handleIndexChange(2)}>표창장</H4>
+                    </TitleTobcontent>
+                </EducationTitleTob>
+                <EducationContent>
+                    <EducationImg>
+                        <Img src={img[index]}/>
+                    </EducationImg>
+                    <ContentRight>
+                        <Edp>우측 상단에 수료증 이수증 표창장을 클릭하면 좌측 이미지가 바뀝니다.</Edp>
+                        {/* <Edp></Edp> */}
+                        <EdImg>
+                            <Img src={아카데미}/>
+                        </EdImg>
+                        <Edp>좌측은 교육기관을 이수한 이수증과 수료증 표창장이 나오는 곳입니다.</Edp>
+                    </ContentRight>
+                </EducationContent>
+                <EducationTitleBottom><H3> &lt; Education</H3></EducationTitleBottom>
+            </AcEducation>
+        </>
+    )
+}
+
+export default Education
---
+++ src/components/Home.js
@@ -0,0 +1,107 @@
+import { useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
+import Head from './Header';
+import About, { Img } from './About';
+import Education from './Education';
+import Portfol from './Portfolio';
+// 공통 부분 --------------------------------------------
+export const Flex = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+// 공통 부분 --------------------------------------------
+export const Flexspacebet = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    /* border-top: 1px solid black; */
+`
+export const Flexcolumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    justify-content: center;
+    gap: 2rem;
+    @media (max-width: 1735px) {
+        padding: 0 1rem;
+    }   
+`
+export const Div = styled.div`
+    text-align: center;
+`
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+export const H1 = styled.h1`
+    font-weight: bold;
+    font-size: 40px;
+    @media (max-width: 1735px) {
+        font-size: 20px;
+    }   
+`;
+
+export const H2 = styled.h2`
+`;
+
+export const H3 = styled.h3`
+    padding-left: 12px;
+    font-size: 30px;
+    font-weight: bold;
+    
+    @media (max-width: 1735px) {
+        font-size: 15px;
+    }   
+`;
+
+export const H4 = styled.h4`
+    font-size: 24px;
+    font-weight: 300;
+    @media (max-width: 1735px) {
+        font-size: 12px;
+    }   
+`;
+export const H5 = styled.h5`
+    font-size: 16px;
+    font-weight: bold;
+    @media (max-width: 1735px) {
+        font-size: 8px;
+    }   
+`;
+
+export const P = styled.p`
+    font-size: 28px;
+    @media (max-width: 1735px) {
+        font-size: 14px;
+    }   
+`;
+
+
+export const Smallp = styled.p`
+    font-size: 24px;
+    color: white;
+    @media (max-width: 1735px) {
+        font-size: 12px;
+    }   
+`; 
+
+
+// ----------------------------------------
+
+
+function Home() {
+    
+    return (
+        <Container>
+            <Head />
+            <About />
+            <Education />
+            <Portfol />
+            <About />
+        </Container>
+    );
+}
+
+export default Home;
